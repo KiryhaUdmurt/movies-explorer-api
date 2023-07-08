5ee9d36f22914bf8e0cdd1b1adfe8fbc5d2fca65
Kirill Kalinin <kalinin1507.2012@gmail.com>
2023-07-08 22:14:28 +0300

    creating pull req

--- utils/constants.js
+++ utils/constants.js
@@ -1,12 +1,12 @@
 const URL_REGEX = /https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+[.]{1,}[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+/i;
 const { DB = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const { PORT = 3000 } = process.env;
-const SERVER_ERR = 500;
 const BAD_REQ = 400;
-const NOTFOUND_ERR = 404;
-const ALREADY_EXISTS = 409;
 const AUTH_ERR = 401;
 const FORBIDDEN_ERR = 403;
+const NOTFOUND_ERR = 404;
+const ALREADY_EXISTS = 409;
+const SERVER_ERR = 500;
 
 module.exports = {
   URL_REGEX,
